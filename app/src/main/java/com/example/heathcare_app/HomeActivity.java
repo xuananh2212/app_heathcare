df7f343f6ef9e2501d3fddbc0cebd2c07a7cf6d0
dat <datthao27042002@gmail.com>
2024-06-09 15:57:50 +0700

    login, regis,detailorder

--- app/src/main/java/com/example/heathcare_app/HomeActivity.java
+++ app/src/main/java/com/example/heathcare_app/HomeActivity.java
@@ -1,8 +1,11 @@
 package com.example.heathcare_app;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,6 +26,29 @@ public class HomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // start goi db
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "").toString();
+        Toast.makeText(getApplicationContext(), "Welcom" + username, Toast.LENGTH_SHORT).show();
+        CardView cardExit = findViewById(R.id.cardExit);
+        cardExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+            }
+        });
+        CardView cardOrderDetails = findViewById(R.id.cardOrderDetails);
+        cardOrderDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, OrderDetailsActivity.class));
+            }
+        });
+        // end goi db
+
         CardView findDoctor = findViewById(R.id.cardFindDoctor);
         findDoctor.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/heathcare_app/OrderDetailsActivity.java
+++ app/src/main/java/com/example/heathcare_app/OrderDetailsActivity.java
@@ -1,6 +1,12 @@
 package com.example.heathcare_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +14,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class OrderDetailsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
 
+public class OrderDetailsActivity extends AppCompatActivity {
+Button btnBack;
+ListView listViewOrderDetail;
+    private ArrayList<HashMap<String, String>> detailList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +30,36 @@ public class OrderDetailsActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btnBack = findViewById(R.id.btnBack);
+        listViewOrderDetail = findViewById(R.id.listViewOrderDetails);
+        detailList = new ArrayList<>();
+        HashMap<String, String> contact1 = new HashMap<>();
+        contact1.put("name", "Nguyen Van Dat");
+        contact1.put("phone", "05869452391");
+        contact1.put("birthDate", "02/02/2002");
+        contact1.put("address", "123 Đường Láng, Đống Đa, Hà Nội");
+        detailList.add(contact1);
+        HashMap<String, String> contact2 = new HashMap<>();
+        contact2.put("name", "Nguyen Van Binh");
+        contact2.put("phone", "0237865483");
+        contact2.put("birthDate", "02/10/2001");
+        contact2.put("address", "654 Trường Chinh, Thanh Xuân, Hà Nội");
+        detailList.add(contact2);
+        SimpleAdapter adapter = new SimpleAdapter(
+                this,
+                detailList,
+                android.R.layout.simple_list_item_2,
+//                android.R.layout.list_item_contact,
+                new String[]{"name", "phone"},
+                new int[]{android.R.id.text1, android.R.id.text2}
+        );
+        listViewOrderDetail.setAdapter(adapter);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(OrderDetailsActivity.this,HomeActivity.class));
+            }
         });
     }
 }
