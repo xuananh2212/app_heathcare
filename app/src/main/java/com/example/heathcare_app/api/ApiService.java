ed1d0b23446a35df80e4157e215d5d536c35a16e
dat <datthao27042002@gmail.com>
2024-06-23 21:55:47 +0700

    fix login

--- app/src/main/java/com/example/heathcare_app/api/ApiService.java
+++ app/src/main/java/com/example/heathcare_app/api/ApiService.java
@@ -1,8 +1,8 @@
 package com.example.heathcare_app.api;
 
-import com.example.heathcare_app.model.Article;
-import com.example.heathcare_app.model.ArticleResponse;
+import com.example.heathcare_app.model.ApiResponse;
 import com.example.heathcare_app.model.LoginResponse;
+import com.example.heathcare_app.model.Metadata;
 import com.example.heathcare_app.model.SignupResponse;
 import com.example.heathcare_app.model.User;
 import com.google.gson.Gson;
@@ -19,14 +19,14 @@ import retrofit2.http.POST;
 public interface ApiService {
     Gson gson = new GsonBuilder().setDateFormat("dd-MM-yyyy").create();
     ApiService apiService = new Retrofit.Builder()
-            .baseUrl("http://192.168.8.1:3052/v1/api/")
+            .baseUrl("http://192.168.0.3:3000/v1/api/")
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
             .create(ApiService.class);
     @POST("auth/signup")
     Call<SignupResponse> signup(@Body User user);
     @POST("auth/login")
-    Call<LoginResponse> login(@Body User user);
+    Call<ApiResponse<Metadata>> login(@Body User user);
     @GET("users")
     Call<List<User>> getListUser();
     @GET("articles")
